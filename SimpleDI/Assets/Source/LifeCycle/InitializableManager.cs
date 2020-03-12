66521df42e93471db5afa7f403698c179aa0bc3c
SheepBro <goattn@gmail.com>
2020-03-12 17:28:14 +0900

    MonoLifeCycle 구현

---
+++ SimpleDI/Assets/Source/LifeCycle/InitializableManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace SimpleDI
+{
+    public class InitializableManager
+    {
+        private List<IInitializable> _initializables = new List<IInitializable>();
+
+        private bool _initialized = false;
+
+        [Inject]
+        public void InitInjections(IInitializable[] initializables)
+        {
+            _initializables.AddRange(initializables);
+        }
+
+        public void Initialize()
+        {
+            Debug.Assert(!_initialized, "InitializableManager.Initialize is called twice.");
+            _initialized = true;
+
+            for (int i = 0; i < _initializables.Count; ++i)
+            {
+                _initializables[i].Initlaize();
+            }
+        }
+    }
+}
